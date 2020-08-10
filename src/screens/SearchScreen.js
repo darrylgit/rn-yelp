fc0861f589edb557bc7fb041a586af39c5c1b6b7
darrylgit <darrylsmithup@gmail.com>
2020-08-10 11:48:35 -0400

    Integrate Yelp API into search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,19 +2,34 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMesssage] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose"
+        }
+      });
+
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMesssage("Something went wrong");
+    }
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term was submitted")}
-      />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
