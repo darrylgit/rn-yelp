1289e4b10a110805517868c0439d9ed81c87b15a
darrylgit <darrylsmithup@gmail.com>
2020-08-10 10:57:19 -0400

    Add local state and event listeners

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name='search' style={styles.iconStyle} />
-      <TextInput placeholder='Search' style={styles.inputStyle} />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        placeholder='Search'
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
