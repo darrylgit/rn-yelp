0ad73bcd7525c9cbc86a6bcd29d31654774cc04a
darrylgit <darrylsmithup@gmail.com>
2020-08-10 15:14:29 -0400

    Pass down id parameter through navigator.navigate

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const id = navigation.getParam("id");
+
+  console.log(id);
   return (
     <View>
       <Text>Results Show</Text>
