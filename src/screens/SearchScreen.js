a2a0b772ac245575b815585f435b51b7cfe3bc97
darrylgit <darrylsmithup@gmail.com>
2020-08-10 12:24:18 -0400

    Filter results by price

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = price => {
+    // price === "$" || "$$" || "$$$"
+    return results.filter(result => result.price === price);
+  };
 
   return (
     <View>
@@ -17,6 +23,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results.</Text>
+      <ResultsList results={filterResultsByPrice("$")} title='Cost Effective' />
+      <ResultsList results={filterResultsByPrice("$$")} title='Bit Pricier' />
+      <ResultsList results={filterResultsByPrice("$$$")} title='Big Spender' />
     </View>
   );
 };
